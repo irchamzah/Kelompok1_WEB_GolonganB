2f8d74d4f5eb74a98f79349f4b324757cd5ecac5
irchamzah <71950891+irchamzah@users.noreply.github.com>
2021-06-05 19:55:32 +0700

    updated

---
+++ resources/views/layouts/layanan.blade.php
@@ -0,0 +1,236 @@
+@extends('layouts.template')
+
+@section('content')
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+      <h1>LAYANAN</h1>
+      <h2>Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper. Proin iaculis purus consequat sem cure digni ssim. Donec porttitora entum.</h2>
+      <div><a href="#about" class="btn-get-started scrollto">Get Started</a></div>
+    </div>
+    <div class="col-lg-6 order-1 order-lg-2 hero-img">
+      <img src="frontend/assets/img/hero-img.png" class="img-fluid" alt="">
+    </div>
+  </div>
+</div>
+
+</section><!-- End Hero -->
+
+<main id="main">
+
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+  <div class="container">
+
+    <div class="row">
+    <H3>TEST</H3>
+      <div class="col-xl-5 col-lg-6 d-flex justify-content-center video-box align-items-stretch">
+        
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+      </div>
+
+      <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+        <h3>LAYANAN</h3>
+        <p>Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus. Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed facere corporis dolores excepturi. Libero laboriosam sint et id nulla tenetur. Suscipit aut voluptate.</p>
+
+        <div class="icon-box">
+          <div class="icon"><i class="bx bx-fingerprint"></i></div>
+          <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+          <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+        </div>
+
+        <div class="icon-box">
+          <div class="icon"><i class="bx bx-gift"></i></div>
+          <h4 class="title"><a href="">Nemo Enim</a></h4>
+          <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+        </div>
+
+        <div class="icon-box">
+          <div class="icon"><i class="bx bx-atom"></i></div>
+          <h4 class="title"><a href="">Dine Pad</a></h4>
+          <p class="description">Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</section><!-- End About Section -->
+
+
+
+<!-- ======= Services Section ======= -->
+<section id="services" class="services section-bg">
+  <div class="container">
+
+    <div class="section-title">
+      <h2>KREASI</h2>
+      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Tutorial membuat Tas</a></h4>
+          <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Dolor Sitema</a></h4>
+          <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6" data-wow-delay="0.1s">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+          <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6" data-wow-delay="0.1s">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Magni Dolores</a></h4>
+          <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6" data-wow-delay="0.2s">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Nemo Enim</a></h4>
+          <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6" data-wow-delay="0.2s">
+        <div class="icon-box">
+          <div class="img-thumbnail"><img src="frontend/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt=""></div>
+          <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
+          <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section><!-- End Services Section -->
+
+
+
+<!-- ======= Team Section ======= -->
+<section id="team" class="team section-bg">
+  <div class="container">
+
+    <div class="section-title">
+      <h2>Team</h2>
+      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <div class="member-img">
+            <img src="frontend/assets/img/team/team-1.jpg" class="img-fluid" alt="">
+            <div class="social">    
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>irchamzah Fikri Ababil</h4>
+            <span>Chief Executive Officer</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <div class="member-img">
+            <img src="frontend/assets/img/team/team-2.jpg" class="img-fluid" alt="">
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Suroto Edy Nur</h4>
+            <span>Product Manager</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <div class="member-img">
+            <img src="frontend/assets/img/team/team-3.jpg" class="img-fluid" alt="">
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Shenila Letishya Ramona</h4>
+            <span>CTO</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <div class="member-img">
+            <img src="frontend/assets/img/team/team-4.jpg" class="img-fluid" alt="">
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Dwi Prasetiyo Utomo</h4>
+            <span>Accountant</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <div class="member-img">
+            <img src="frontend/assets/img/team/team-5.jpg" class="img-fluid" alt="">
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Muhammad Afdal Zikri</h4>
+            <span>Accountant</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Team Section -->
+
+
+</main>
+<!-- End #main -->
+
+@endsection
