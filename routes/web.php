adb72d52b5279a76001cd2d4063ee7967ddb4883
irchamzah <71950891+irchamzah@users.noreply.github.com>
2021-06-12 21:34:41 +0700

    upload file admin

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::get('/notifikasi', 'App\Http\Controllers\NotifikasiController@notifikasi'
 
 Route::get('/profil', 'App\Http\Controllers\ProfilController@profil');
 
+Route::get('/admin', 'App\Http\Controllers\AdminController@index');
+
 
 // Route::get('/login', function() {
 //     return view('/layouts/login');
