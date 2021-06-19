884583aa199267fe9a6b1dd93b1b4515d35f9f48
irchamzah <71950891+irchamzah@users.noreply.github.com>
2021-06-19 18:16:43 +0700

    bisa update profil

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use Hash;
+use App\Models\User;
 
 class ProfilController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        return view('profil.profil');
+        $user = User::where('id', Auth::user()->id)->first();
+
+        return view('profil.profil', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'password' => 'confirmed',
+        ]);
+
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->alamat = $request->alamat;
+        $user->nohp = $request->nohp;
+        if(!empty($request->password))
+        {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->update();
+
+        return redirect('profil')->with('success', 'Update Profile Sukses!');;
     }
 }
--- resources/views/profil/profil.blade.php
+++ resources/views/profil/profil.blade.php
@@ -1,51 +1,155 @@
 @extends('layouts.front.app')
 
 @section('content')
-<!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex align-items-center">
 
-<div class="container">
-  <div class="row">
-    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-      <h1>PROFIL</h1>
-      <h2>buat membantu coba cari referensi, tampilan profil biasanya kyk gimana, terus utk lengkapnya liat tutorialnya di <a href="https://www.w3schools.com/bootstrap4/default.asp">sini</a></h2>
-      <div><a href="#about" class="btn-get-started scrollto">Get Started</a></div>
-    </div>
-    <div class="col-lg-6 order-1 order-lg-2 hero-img">
-      <img src="frontend/assets/img/hero-img.png" class="img-fluid" alt="">
-    </div>
-  </div>
-</div>
-
-</section><!-- End Hero -->
-
-<main id="main">
-
-<!-- ======= About Section ======= -->
-<section id="about" class="about">
+<section id="services" class="services section-bg">
   <div class="container">
-
     <div class="row">
-      
-      <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
-        <h3>INFORMASI SAMPAH</h3>
-        <p>Pilih Jenis Sampah yang akan dijual.</p>
-
-       
-
-
-
+      <div class="col-md-15">
+        <a href="{{ url('home') }}" class="btn btn-primary"><i>Kembali</i></a>
+      </div><br><br><br>
+     
+      <!-- <div class="col-md-12 mt-2">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ url('/') }} "> Home </a></li>
+            <li class="breadcrumb-item active" aria-current="page"> Profile </li>
+          </ol>
+        </nav>
+      </div> -->
+      <div class="col-md-12">
+      @if(Session::has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{Session::get('success')}}
+                </div>
+            @endif
+        <div class="card">
+          <div class="card-body">
+            <h4>My Profile</h4>
+            <table class="table">
+              <tbody>
+                <tr>
+                  <td width="200">Nama</td>
+                  <td width="10">:</td>
+                  <td>{{ $user->name }}</td>
+                </tr>
+                <tr>
+                  <td>Email</td>
+                  <td>:</td>
+                  <td>{{ $user->email }}</td>
+                </tr>
+                <tr>
+                  <td>Alamat</td>
+                  <td>:</td>
+                  <td>{{ $user->alamat }}</td>
+                </tr>
+                <tr>
+                  <td>No.HP</td>
+                  <td>:</td>
+                  <td>{{ $user->nohp }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
+      <div class="col-md-12 mt-4">
+        <div class="card">
+          <div class="card-body">
+            <h4>Edit Profile</h4>
+            <form method="POST" action="{{ url('profil') }}">
+                        @csrf
 
+                        <div class="form-group row">
+                            <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('Name') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
 
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-2 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="alamat" class="col-md-2 col-form-label text-md-right">{{ __('Alamat') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea id="alamat" type="alamat" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ $user->alamat }}" required autocomplete="alamat">{{ $user->alamat }}</textarea>
+
+                                @error('alamat')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nohp" class="col-md-2 col-form-label text-md-right">{{ __('No. HP') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nohp" type="nohp" class="form-control @error('nohp') is-invalid @enderror" name="nohp" value="{{ $user->nohp }}" required autocomplete="nohp">
+
+                                @error('nohp')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-2 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-2 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-2">
+                                <button type="submit" class="btn btn-primary">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</section><!-- End About Section -->
-
-  
-
-</main>
-<!-- End #main -->
-
+</section>
 @endsection
