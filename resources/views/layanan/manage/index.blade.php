ec9cdbaa00a6c49413d63beb1f82ef00923ac759
irchamzah <71950891+irchamzah@users.noreply.github.com>
2021-07-23 00:20:11 +0700

    tabel di admin sdh rapi

--- resources/views/layanan/manage/index.blade.php
+++ resources/views/layanan/manage/index.blade.php
@@ -31,7 +31,7 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Cara Memesan</h3>
+                                <h3 class="card-title">Proses Pemesanan</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
@@ -41,23 +41,13 @@
                                         <div class="card-header">
                                             <h4 class="card-title w-100">
                                                 <a class="d-block w-100" data-toggle="collapse" href="#collapseOne">
-                                                    Collapsible Group Item #1
+                                                    1. Klik Tombol "Buat Pesanan Baru" pada Tabel Dibawah
                                                 </a>
                                             </h4>
                                         </div>
                                         <div id="collapseOne" class="collapse show" data-parent="#accordion">
                                             <div class="card-body">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                                                3
-                                                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                                                laborum
-                                                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
-                                                nulla
-                                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                                                beer
-                                                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                                labore sustainable VHS.
+                                                Setelah itu, Silahkan pilih Kategori Sampah dan Foto Seluruh Sampahmu yang akan Dijual (Foto Harus Jelas). Pilih Tanggal Jemput Sampah (Pemesan atau Penanggung Jawab Harus ada pada tanggal yang dipilih). Lalu Isi Keterangan (Pesan) untuk Penjemput Sampah.
                                             </div>
                                         </div>
                                     </div>
@@ -65,23 +55,13 @@
                                         <div class="card-header">
                                             <h4 class="card-title w-100">
                                                 <a class="d-block w-100" data-toggle="collapse" href="#collapseTwo">
-                                                    Collapsible Group Danger
+                                                    2. Kami Akan Memeriksa Pesanan yang Dikirim
                                                 </a>
                                             </h4>
                                         </div>
                                         <div id="collapseTwo" class="collapse" data-parent="#accordion">
                                             <div class="card-body">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                                                3
-                                                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                                                laborum
-                                                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
-                                                nulla
-                                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                                                beer
-                                                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                                labore sustainable VHS.
+                                                Sebelum Kami tentukan, Pemesan dapat Membatalkan Pesanan. Jika Sudah Dikonfirmasi, Pesanan tidak Dapat Dibatalkan. Kami yang akan menentukan pesanan tersebut layak Diterima atau Ditolak tergantung Isi Pesanan dan Situasi. Jika Diterima, Kami akan Segera Menjemput Sampah Sesuai Tanggal yang ada pada Pesanan. Jika Ditolak, Kami akan Memberikan Alasannya. Alasan Penolakan akan muncul pada Fitur Notifikasi.
                                             </div>
                                         </div>
                                     </div>
@@ -89,23 +69,13 @@
                                         <div class="card-header">
                                             <h4 class="card-title w-100">
                                                 <a class="d-block w-100" data-toggle="collapse" href="#collapseThree">
-                                                    Collapsible Group Success
+                                                    3. Pembayaran Berlangsung Ditempat
                                                 </a>
                                             </h4>
                                         </div>
                                         <div id="collapseThree" class="collapse" data-parent="#accordion">
                                             <div class="card-body">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                                                3
-                                                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                                                laborum
-                                                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
-                                                nulla
-                                                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                                                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                                                beer
-                                                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                                labore sustainable VHS.
+                                                Setelah Harga Sampah Ditentukan, Kami akan Membayarnya Langsung Ditempat, Atau Biasa Disebut Cash On Delivery (COD).
                                             </div>
                                         </div>
                                     </div>
